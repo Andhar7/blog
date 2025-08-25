37942708f903bc818dcffc11904b63df96d4433d
Andhar7 <boxingprofi@web.de>
2025-08-25 11:08:54 +0200

    changed for simple html way plus context_proc

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,12 +3,12 @@ from blogs.models import Blog, Category
 
 
 def home(request):
-    categories = Category.objects.all()
+    # categories = Category.objects.all() # Moved to context processor
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     # featured_posts = Blog.objects.filter(is_featured=True)
     posts = Blog.objects.filter(is_featured=False, status='Published') 
     context = {
-        'categories': categories,
+        # 'categories': categories, # Moved to context processor
         'featured_posts': featured_posts,
         'posts': posts
     }
